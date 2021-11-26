84ac771426b9fd3e2bab3199ec89c9f39bc13e06
siobhanlgorman <siobhanlgorman@gmail.com>
2021-11-26 08:56:19 +0000

    Initial commit

---
+++ knits_and_pieces/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for knits_and_pieces project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'knits_and_pieces.settings')
+
+application = get_wsgi_application()
