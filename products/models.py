e37b24ddbe1e89499ee99d65e21fd25dc0b3a096
siobhanlgorman <siobhanlgorman@gmail.com>
2021-12-07 18:52:57 +0000

    Continue product model

--- products/models.py
+++ products/models.py
@@ -11,3 +11,41 @@ class Category(models.Model):
 
     def get_friendly_name(self):
         return self.friendly_name
+
+
+class Tag(models.Model):
+    """A model for a tag for a special addition to a product"""
+
+
+class Product(models.Model):
+    """A model for the product"""
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=100)
+    ITEM_SIZES = (
+        ('os', 'One Size'),
+        ('xs', 'Extra-Small'),
+        ('s', 'Small'),
+        ('m', 'Medium'),
+        ('l', 'Large'),
+        ('xl', 'Extra-Large'),
+    )
+    size = models.CharField(max_length=100, choices=ITEM_SIZES)
+    description = models.TextField()
+    tag = models.ForeignKey(
+        'Tag', null=True, blank=True, on_delete=models.SET_NULL, max_length=99)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    material = models.CharField(max_length=100)
+    machine_wash = models.BooleanField(default=True)
+    product_image1 = models.ImageField()
+    product_image1 = models.ImageField(null=True, blank=True)
+    product_image1 = models.ImageField(null=True, blank=True)
+    shade_type = models.CharField(max_length=50, blank=True, default="")
+    colour1 = models.CharField(max_length=50, blank=True, default="")
+    colour1 = models.CharField(max_length=50, blank=True, default="")
+    colour1 = models.CharField(max_length=50, blank=True, default="")
+    colour1 = models.CharField(max_length=50, blank=True, default="")
+    pattern_name = models.CharField(max_length=100, blank=True, default="")
+    designer = models.CharField(max_length=50, blank=True, default="")
+    can_custom_design = models.BooleanField(default=False)
+    likes = models.ManyToManyField('User')
