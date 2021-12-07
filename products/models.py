d12676d8b86fba2dddb1c50304409add1c711b06
siobhanlgorman <siobhanlgorman@gmail.com>
2021-12-07 16:06:26 +0000

    Create model for product category

--- products/models.py
+++ products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """A model for the product category"""
+    name = models.CharField(max_length=80)
+    friendly_name = models.CharField(max_length=80, blank=True, default="")
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
