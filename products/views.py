7c6461d8c5aaeed67fbaa3d0d7e8aa4e6ee2b521
siobhanlgorman <siobhanlgorman@gmail.com>
2021-12-09 16:44:07 +0000

    Create product_detail template and update views and urls

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 """Imports"""
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 # Create your views here.
 
@@ -13,3 +13,14 @@ def all_products(request):
         'products': products,
     }
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show the product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+    return render(request, 'products/product_detail.html', context)
