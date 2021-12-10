ee572202b72eb1c3a9ddaa035254bcbcfaa17561
siobhanlgorman <siobhanlgorman@gmail.com>
2021-12-10 15:46:37 +0000

    Add seaech query function to all products view

--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
 """Imports"""
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 # Create your views here.
 
@@ -8,10 +10,24 @@ def all_products(request):
     """ A view to show all products including search and sort queries """
 
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET['q']:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "Please enter your search query!")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
 
     context = {
         'products': products,
+        'search_term': query,
     }
+
     return render(request, 'products/products.html', context)
 
 
