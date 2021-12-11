b93bef5b1ab801d436dd3463c433b8b7ab39a38a
siobhanlgorman <siobhanlgorman@gmail.com>
2021-12-11 11:31:45 +0000

    Add category filters to shop links in footer

--- products/views.py
+++ products/views.py
@@ -25,11 +25,11 @@ def all_products(request):
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
 
-        if 'direction' in request.GET:
-            direction = request.GET['direction']
-            if direction == 'desc':
-                sortkey = f'-{sortkey}'
-        products = products.order_by(sortkey)
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
 
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
