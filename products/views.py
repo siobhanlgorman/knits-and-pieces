b3276e33998f7ecfa6e2c4ea6fa8d6b5381170cd
siobhanlgorman <siobhanlgorman@gmail.com>
2021-12-12 13:17:54 +0000

    Complete functionality to sort by price name and category from shop dropdown menu

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Category, Tag
 # Create your views here.
 
