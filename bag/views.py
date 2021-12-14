9696d0bd5fc8cbc6e7e0c40ee44a5a752e1853aa
siobhanlgorman <siobhanlgorman@gmail.com>
2021-12-14 09:55:37 +0000

    Add function to update quantity in bag in views.py

--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,5 @@
 """Imports"""
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -22,3 +22,18 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+            bag[item_id] = quantity
+    else:
+        bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
