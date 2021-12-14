9b9ed63fc759be5871ad1f9c0f98fab08b5c4ac0
siobhanlgorman <siobhanlgorman@gmail.com>
2021-12-14 17:30:42 +0000

    Fix error in toast not displaying for updated quantity

--- bag/views.py
+++ bag/views.py
@@ -1,9 +1,10 @@
 """Imports"""
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
-from products.models import Product
-# Create your views here.
 
+from products.models import Product
+
+# Create your views here.
 
 def view_bag(request):
     """ A view to render the bag contents page """
@@ -12,18 +13,17 @@ def view_bag(request):
 
 def add_to_bag(request, item_id):
     """Add a quantity of the specified product to the shopping bag"""
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
     else:
         bag[item_id] = quantity
-        print('hello')
         messages.success(request, f'You added {product.name} to your basket')
-        print('hello again')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
@@ -31,14 +31,17 @@ def add_to_bag(request, item_id):
 
 def adjust_bag(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
+    product = get_object_or_404(Product, pk=item_id)
 
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
 
     if quantity > 0:
         bag[item_id] = quantity
+        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
     else:
         bag.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your basket')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
