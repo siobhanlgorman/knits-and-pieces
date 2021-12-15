c79c50798317c4b6b2a987b63d82441b48696ea1
siobhanlgorman <siobhanlgorman@gmail.com>
2021-12-15 12:45:50 +0000

    Add forms

--- checkout/apps.py
+++ checkout/apps.py
@@ -1,7 +1,9 @@
+"""Imports"""
 from django.apps import AppConfig
 
 
 class CheckoutConfig(AppConfig):
+    """override the ready method and import signals.py"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'checkout'
 
