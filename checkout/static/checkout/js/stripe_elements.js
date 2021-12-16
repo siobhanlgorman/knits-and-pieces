cec817bcb1488c393d920ebf4c7486e254138363
siobhanlgorman <siobhanlgorman@gmail.com>
2021-12-16 14:10:21 +0000

    Install and import stripe

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,15 +1,13 @@
 /*
     Core logic/payment flow for this comes from here:
-    https://stripe.com/docs/payments/accept-a-payment
-    CSS from here: 
-    https://stripe.com/docs/stripe-js
+    https://stripe.com/docs/payments and Boutique Ado walkthrough
 */
 
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripe_public_key);
-var elements = stripe.elements();
-var style = {
+const stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
+const client_secret = $('#id_client_secret').text().slice(1, -1);
+const stripe = Stripe(stripe_public_key);
+const elements = stripe.elements();
+const style = {
     base: {
         color: '#000',
         fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
@@ -24,5 +22,21 @@ var style = {
         iconColor: '#dc3545'
     }
 };
-var card = elements.create('card', {style: style});
+const card = elements.create('card', {style: style});
 card.mount('#card-element');
+
+// Handle realtime validation errors on the card element
+card.addEventListener('change', function (event) {
+    const errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        const html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
--- checkout/views.py
+++ checkout/views.py
@@ -1,15 +1,26 @@
 """Imports"""
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
+from bag.contexts import bag_contents
 from .forms import OrderForm
 
+import stripe
 
 def checkout(request):
+    """
+    Gets the contents of the shopping bag and
+    calculate the total for stripe payments
+    """
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "Your shopping basket is empty!")
         return redirect(reverse('products'))
+
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
