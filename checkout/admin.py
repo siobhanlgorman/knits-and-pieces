ef51de34e5aac5c0f451a4c813a1e7c5d958c789
siobhanlgorman <siobhanlgorman@gmail.com>
2021-12-17 16:35:40 +0000

    Complete webhook funcions

--- checkout/admin.py
+++ checkout/admin.py
@@ -21,13 +21,13 @@ class OrderAdmin(admin.ModelAdmin):
 
     readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
-                       'grand_total',)
+                       'grand_total', 'original_bag', 'stripe_pid')
 
     fields = ('order_number', 'date', 'first_name', 'last_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county', 'delivery_cost',
-              'order_total', 'grand_total',)
+              'order_total', 'grand_total', 'original_bag', 'stripe_pid')
 
     list_display = ('order_number', 'date', 'first_name', 'last_name',
                     'order_total', 'delivery_cost',
