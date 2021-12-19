8a1715652b4fd372e5a232ae56ca73e5c7fbb43a
siobhanlgorman <siobhanlgorman@gmail.com>
2021-12-19 11:34:51 +0000

    Update countryfield default colours in profile form

---
+++ profiles/forms.py
@@ -0,0 +1,40 @@
+"""Imports"""
+from django import forms
+from .models import UserProfile
+
+
+class UserProfileForm(forms.ModelForm):
+    """
+    Fields for the order form
+    """
+    class Meta:
+        """Sets the fields to be used in the order form"""
+        model = UserProfile
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'default_email': 'Email Address',
+            'default_phone_number': 'Phone Number',
+            'default_postcode': 'Postal Code',
+            'default_town_or_city': 'Town or City',
+            'default_street_address1': 'Street Address 1',
+            'default_street_address2': 'Street Address 2',
+            'default_county': 'County',
+        }
+
+        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'default_country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'profile-form-input'
+            self.fields[field].label = False
