c4c563a9a8ef0122d12a15ba9c326494a81e0c72
siobhanlgorman <siobhanlgorman@gmail.com>
2021-12-21 11:45:41 +0000

    Add views url and template for delete product in product management

--- products/views.py
+++ products/views.py
@@ -82,9 +82,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            product = form.save()
+            messages.success(request, 'Successfully added {{ product.name }}!')
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             print('error message')
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')
@@ -106,7 +106,7 @@ def edit_product(request, product_id):
         form = ProductForm(request.POST, request.FILES, instance=product)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Successfully updated product!')
+            messages.success(request, f'{product.name} successfully updated!')
             return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to update product. Please ensure the form is valid.')
@@ -121,3 +121,10 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, f'{product.name}deleted!')
+    return redirect(reverse('products'))
