50fbfb61039b43541dc89f1da76abcc8cfe4540e
siobhanlgorman <siobhanlgorman@gmail.com>
2022-01-18 19:58:01 +0000

    Add screenshots to testing.md

--- checkout/views.py
+++ checkout/views.py
@@ -135,7 +135,6 @@ def checkout(request):
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
         'client_secret': intent.client_secret,
-
     }
 
     return render(request, template, context)
